ecdef5d77ed0ecfbb88c7700025d66dfaebeb87f
xiaoxiaoqian1217 <xiaoxiaoqian581217>
2023-04-05 22:50:28 +0800

    feat:添加图标

--- src/layouts/basic-layout/index.vue
+++ src/layouts/basic-layout/index.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="global-nav ">basic-layout</div>
-  <Button type="primary">Primary Button</Button>
+  <div>
+    <!--global-header -->
+    <div class="global-nav min-h-48px">
+      <div>
+        <div></div>
+        <div>
+          <div>独立应用</div>
+          <span>
+            <ExclamationCircleOutlined />
+          </span>
+          <span>
+            <StarOutlined />
+          </span>
+        </div>
+      </div>
 
+    </div>
+  </div>
+
+  <!-- <Button type="primary">Primary Button</Button> -->
 </template>
 
 <script setup lang="ts">
+import {
+  ExclamationCircleOutlined,
+  StarOutlined
+} from '@ant-design/icons-vue';
 import { Button } from "ant-design-vue";
 </script>
 
