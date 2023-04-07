004e30df88f5ab635d38d1dbae28158c3cae9375
xiaoxiaoqian <785328721@qq.com>
2023-04-07 18:50:44 +0800

    feat: 添加login页面

---
+++ src/components/todo-list.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div>{{ title }}</div>
+    <div>
+      <ul>
+        <li v-for="task in list" :key="task.id">
+          {{ item }}
+        </li>
+      </ul>
+      <!-- 任务状态为开发中时可以添加 -->
+      <div v-if="status === Status.new" class="w-272px">
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  enum Status {
+    new = 1,
+    doing = 2,
+    solve = 3,
+    feedback = 4,
+    close = 5,
+    reject = 6,
+  }
+  interface TaskItem {
+    author: number;
+    description: string;
+    id: number;
+    priority: number;
+    status: number;
+    subject: string;
+    tracker: number;
+  }
+  interface TaskListProps {
+    title: string;
+    list: TaskItem[];
+    status: Status;
+  }
+
+  const props = withDefaults(defineProps<TaskListProps>(), {});
+  const { title, list, status } = props;
+</script>
+
+<style scoped></style>
