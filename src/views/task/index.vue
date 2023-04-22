c9a5b82e8d8be056253130419173c85e2e47806e
xiaoxiaoqian1217 <xiaoxiaoqian581217>
2023-04-22 22:58:58 +0800

    feat: 调整样式

--- src/views/task/index.vue
+++ src/views/task/index.vue
@@ -336,7 +336,7 @@
     /* width: calc(100vw - 380px); */
   }
   .task-sidebar {
-    box-shadow: 0px 10px 24px rgba(0, 0, 0, 0.1);
+    border-right: 1px solid #e5e7eb;
     height: calc(100vh - 85px);
     bottom: 0;
     left: 0;
@@ -376,6 +376,7 @@
     transform: translateX(-400px);
     z-index: 30;
     cursor: pointer;
+    box-shadow: 0px 10px 24px rgba(0, 0, 0, 0.1);
   }
   .task-list-handler:hover .task-list-panel {
     transform: translateX(-20px);
